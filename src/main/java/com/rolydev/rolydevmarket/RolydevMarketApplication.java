82d9d8d3a66f0b2bb606b2cdde4c23cad0649cd2
Rolando Mamani Salas <rolando.mamani@fundacion-jala.org>
2022-05-19 06:05:08 -0400

    fist commit

---
+++ src/main/java/com/rolydev/rolydevmarket/RolydevMarketApplication.java
@@ -0,0 +1,13 @@
+package com.rolydev.rolydevmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolydevMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolydevMarketApplication.class, args);
+	}
+
+}
